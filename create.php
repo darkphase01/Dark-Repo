248428d6530b760d6fe6e7ec091c04bc954f7e8e
darkphase01 <ronaldmuzares@gmail.com>
2015-10-07 17:48:34 +0800

    create-modified
    create-modified

--- create.php
+++ create.php
@@ -5,12 +5,12 @@
 <?php echo form_open('news/create') ?>
 
     <label for="title">Title</label>
-    <input type="input" name="title" /><br />
+    <input type="input" name="title" value="Sample" /><br />
 
     <label for="text">Text</label>
     <textarea name="text"></textarea><br />
 
-    <input type="submit" name="submit" value="Create news item" />
+    
 
 <?php echo form_close(); ?>
 <a href="/ci30/news/">Home</a>
